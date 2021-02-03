d09ca92fb64204d2ca463ad185fbe8757f2feb34
s-justina <jus.sobcz@gmail.com>
2021-02-03 22:01:08 +0100

    create mongodb collection and connect to todo app

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,24 +1,53 @@
 var express = require("express");
+var mongoose = require("mongoose");
 
-var data = [
-  { item: "get milk" },
-  { item: "go walk" },
-  { item: "kick some coding ass" },
-];
+// Connect to the database
+mongoose.connect(
+  "mongodb+srv://test:test@test.slkch.mongodb.net/test?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+// Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+  item: String,
+});
+
+var Todo = mongoose.model("Todo", todoSchema);
+
+// var data = [
+//   { item: "get milk" },
+//   { item: "go walk" },
+//   { item: "kick some coding ass" },
+// ];
 var urlencodedParser = express.urlencoded({ extended: false });
 module.exports = function (app) {
   app.get("/todo", function (req, res) {
-    res.render("todo", { todos: data });
+    // get data from mongodb and pass it to the view
+    Todo.find({}, function (err, data) {
+      if (err) throw err;
+      res.render("todo", { todos: data });
+    });
   });
   app.post("/todo", urlencodedParser, function (req, res) {
-    data.push(req.body);
-    res.json(data);
+    //get data from the view and add it to the mongodb
+    var newTodo = Todo(req.body).save(function (err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
   });
 
   app.delete("/todo/:item", function (req, res) {
-    data = data.filter(function (todo) {
-      return "-" + todo.item.replace(/ /g, "-") !== req.params.item;
+    // delete requestet item from mongodb
+    Todo.find({
+      item: Array.from(req.params.item).splice(1).join("").replace(/-/g, " "),
+    }).remove(function (err, data) {
+      if (err) throw err;
+      res.json(data);
     });
-    res.json(data);
+    //old way to remove item without mongodb database
+    // data = data.filter(function (todo) {
+    //   return "-" + todo.item.replace(/ /g, "-") !== req.params.item;
+    // });
+    // res.json(data);
   });
 };
